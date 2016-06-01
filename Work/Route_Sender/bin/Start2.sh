2da79ba90931fcf38c89f8061d300c35e4880264
nomor <nomor@sim22.(none)>
2016-06-01 12:41:53 +0200

    1. FDT is updated to EFDT 2. Service Layer Signalling using MPD and S-TSID in separate LCT channel

--- Work/Route_Sender/bin/Start2.sh
+++ Work/Route_Sender/bin/Start2.sh
@@ -7,13 +7,13 @@
 #Define Directories
 #DASHContent=ToS720p_0_5
 #DASHContent2=Elysium720p_0_5
-DASHContent=ToSLC_0_5
-DASHContent2=ElysiumLC_0_5
+DASHContent=Elysium_1_0
+#DASHContent2=ElysiumLC_0_5
 
 #if [ "$#" -gt 0 ] && [ "$1" -eq 1000 ]
 #then
-  DASHContent=Hotel
-  DASHContent2=Wave
+ # DASHContent=Hotel
+  #DASHContent2=Wave
 #fi
 
 FLUTESender=.
@@ -24,26 +24,31 @@ Delay2=1.75
 #x=10					#FLUTE receiver will be started after Delay - x seconds
 bitRate=50000			#Bitrate in kb/s to be used in FLUTE Sender
 
-fdtVid=fdt_Video.xml
-fdtAud=fdt_Audio.xml
+fdtVid=efdt_Video.xml
+fdtAud=efdt_Audio.xml
+fdtMPD=efdt_MPD.xml
 
 FLUTEVideoInput="FluteInput_Video.txt"
 FLUTEAudioInput="FluteInput_Audio.txt"
 
 sdp=SDP1.sdp
 sdp2=SDP2.sdp					#SDP to be used by sender
-
 sdp3=SDP3.sdp
-sdp4=SDP4.sdp					#SDP to be used by sender	
+
+sdp4=SDP4.sdp
+sdp5=SDP5.sdp					#SDP to be used by sender	
+sdp6=SDP6.sdp
 
 encodingSymbolsPerPacket=1		#A value of zero indicates that different chunks of segment have different delay
 								#and maximum transmission unit size is used (e.g. 1500 bytes per packet)
 
-Log=Send_Log_Video.txt			#Log containing delays corresponding to FLUTE server
-Log2=Send_Log_Audio.txt			#Log containing delays corresponding to FLUTE server
+Log=Send_Log_MPD.txt			#Log containing delays corresponding to FLUTE server
+Log2=Send_Log_Video.txt			#Log containing delays corresponding to FLUTE server
+Log3=Send_Log_Audio.txt
 
-Log3=Send_Log_Video2.txt			#Log containing delays corresponding to FLUTE server
-Log4=Send_Log_Audio2.txt			#Log containing delays corresponding to FLUTE server
+Log4=Send_Log_MPD2.txt			#Log containing delays corresponding to FLUTE server
+Log5=Send_Log_Video2.txt			#Log containing delays corresponding to FLUTE server
+Log6=Send_Log_Audio2.txt
 
 [ $encodingSymbolsPerPacket -lt 0 ] && echo "The number of encoding symbols should be zero or greater" && exit 
 
@@ -52,10 +57,10 @@ echo "Converting MPD"
 
 #Brackets are used to temporarilSimAdminy change working directory
 ./ConvertMPD.sh $DASHContent MultiRate.mpd $Delay
-./ConvertMPD.sh $DASHContent2 MultiRate.mpd $Delay2
+#./ConvertMPD.sh $DASHContent2 MultiRate.mpd $Delay2
 
 chmod 777 $DASHContent/*
-chmod 777 $DASHContent2/*
+#chmod 777 $DASHContent2/*
 
 echo "Done"
 
@@ -70,10 +75,12 @@ killall flute_sender
 
 
 #(cd $FLUTESender && ./flute -S -r:$bitRate -B:$DASHContent -Q -f:$fdtVid -m:224.1.1.1 -p:4000 -t:1 -v:4 -y:$videoSegDur -Y:$videoSegDur -J:$Log)
-(cd $FLUTESender && ./flute_sender -S -r:$bitRate -B:$DASHContent -f:$DASHContent/$fdtVid -d:$sdp  -y:$DASHContent/$FLUTEVideoInput -Y:$encodingSymbolsPerPacket -J:$Log&)
-(cd $FLUTESender && ./flute_sender -S -r:$bitRate -B:$DASHContent -f:$DASHContent/$fdtAud -d:$sdp2 -y:$DASHContent/$FLUTEAudioInput -Y:$encodingSymbolsPerPacket -J:$Log2&)
+(cd $FLUTESender && ./flute_sender -S -r:$bitRate -B:$DASHContent -f:$DASHContent/$fdtMPD -d:$sdp -Y:$encodingSymbolsPerPacket -J:$Log -C&)
+(cd $FLUTESender && ./flute_sender -S -r:$bitRate -B:$DASHContent -f:$DASHContent/$fdtVid -d:$sdp2 -y:$DASHContent/$FLUTEVideoInput -Y:$encodingSymbolsPerPacket -J:$Log2&)
+(cd $FLUTESender && ./flute_sender -S -r:$bitRate -B:$DASHContent -f:$DASHContent/$fdtAud -d:$sdp3 -y:$DASHContent/$FLUTEAudioInput -Y:$encodingSymbolsPerPacket -J:$Log3&)
 
 #Sending of second video
-(cd $FLUTESender && ./flute_sender -S -r:$bitRate -B:$DASHContent2 -f:$DASHContent2/$fdtVid -d:$sdp3 -y:$DASHContent2/$FLUTEVideoInput -Y:$encodingSymbolsPerPacket -J:$Log3&)
-(cd $FLUTESender && ./flute_sender -S -r:$bitRate -B:$DASHContent2 -f:$DASHContent2/$fdtAud -d:$sdp4 -y:$DASHContent2/$FLUTEAudioInput -Y:$encodingSymbolsPerPacket -J:$Log4 && fg)
+#(cd $FLUTESender && ./flute_sender -S -r:$bitRate -B:$DASHContent -f:$DASHContent/$fdtMPD -d:$sdp4 -Y:$encodingSymbolsPerPacket -J:$Log4&)
+#(cd $FLUTESender && ./flute_sender -S -r:$bitRate -B:$DASHContent2 -f:$DASHContent2/$fdtVid -d:$sdp5 -y:$DASHContent2/$FLUTEVideoInput -Y:$encodingSymbolsPerPacket -J:$Log5&)
+#(cd $FLUTESender && ./flute_sender -S -r:$bitRate -B:$DASHContent2 -f:$DASHContent2/$fdtAud -d:$sdp6 -y:$DASHContent2/$FLUTEAudioInput -Y:$encodingSymbolsPerPacket -J:$Log6 && fg)
 
